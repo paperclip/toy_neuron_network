95566b7849224e4fedf5bc476f93e19bfcb3e9e5
paperclip <github@leeder.plus.com>
2019-12-05 19:49:49 +0000

    Attempt to implement back-propagation
    I think limiting by the true function allowed to go negative, whereas relu can't go negative.

--- network.py
+++ network.py
@@ -10,6 +10,8 @@ import neuron
 def target_function(input):
     return input[0] + input[1] - input[2]
 
+LEARNING_RATE = 0.001
+
 class Network(object):
     def __init__(self):
         self.__m_layer1 = (
@@ -17,17 +19,24 @@ class Network(object):
             neuron.Neuron([1,3,4],5),
             neuron.Neuron([1,2,3],5),
         )
-        self.__m_layer2 = neuron.Neuron([1,2,3],3)
+        self.__m_layer2 = [ neuron.Neuron([1,2,3],3) ]
 
     def forward_evaluate(self, inputs):
         layer1_outputs = [ n.forward_evaluate(inputs) for n in self.__m_layer1 ]
         # print(layer1_outputs)
-        layer2_output = self.__m_layer2.forward_evaluate(layer1_outputs)
+        layer2_output = self.__m_layer2[0].forward_evaluate(layer1_outputs)
         # print(layer2_output)
         return layer2_output
 
-    def back_propagate(self, inputs, predicted, error):
-        pass
+    def back_propagate(self, actual):
+        for n in self.__m_layer2:
+            n.back_propogate(actual)
+        for i, n in enumerate(self.__m_layer1):
+            n.back_propograte_hidden(self.__m_layer2, i)
+
+    def printout(self):
+        print("LAYER1:",[ n.weights() for n in self.__m_layer1])
+        print("LAYER2:", [n.weights() for n in self.__m_layer2])
 
 def generate_input():
     while True:
@@ -36,14 +45,38 @@ def generate_input():
 def square_error(predicted, actual):
     return (predicted - actual) ** 2
 
+def train_batch(network, training_function):
+    batch_size = 10
+
+    while True:
+        n = 0
+        sum_of_error = 0.0
+        for i in generate_input():
+            actual = training_function(i)
+            predicted = network.forward_evaluate(i)
+            error = square_error(predicted, actual)
+            sum_of_error += error
+            n += 1
+            if n > batch_size:
+                break
+        
+        average_square_error = sum_of_error / n
+        network.back_propagate(i, average_square_error)
+        time.sleep(0.1)
+
 def train(network, training_function):
+    iteration = 0
     for i in generate_input():
+        print(iteration)
+        iteration += 1
         actual = training_function(i)
         predicted = network.forward_evaluate(i)
-        error = square_error(predicted, actual)
-        print(i,actual,predicted,error)
-        network.back_propagate(i, predicted, error)
+        print(i,"->",predicted,"should be",actual)
+        network.back_propagate(actual)
+        network.printout()
+        print()
         time.sleep(0.1)
+
 
 def main(argv):
     network = Network()
--- neuron.py
+++ neuron.py
@@ -6,16 +6,71 @@ def relu(output):
     return output
 
 
+def relu_prime(z):
+    if z > 0:
+        return 1
+    return 0
+
+
+def cost(yHat, y):
+    return 0.5 * (yHat - y)**2
+
+
+def cost_prime(yHat, y):
+    return yHat - y
+
+
 class Neuron(object):
     def __init__(self, weights, bias):
         self.__m_weights = weights
         self.__m_activation = relu
+        self.__m_activation_prime = relu_prime
         self.__m_bias = bias
+        self.__m_learning_rate = 0.001
 
     def __weighted_sum(self, inputs):
         w = self.__m_weights
         assert len(w) == len(inputs)
         return sum((w[i] * inputs[i] for i in range(len(w)) ))
 
+    def weight(self, pos):
+        return self.__m_weights[pos]
+
+    def weights(self):
+        return self.__m_weights
+
     def forward_evaluate(self, inputs):
-        return self.__m_activation(self.__weighted_sum(inputs) + self.__m_bias)
+        self.__m_most_recent_inputs = inputs
+        output = self.__m_activation(self.__weighted_sum(inputs) + self.__m_bias)
+        self.__m_most_recent_output = output
+        return output
+
+    def __apply_error(self, error):
+        error *= self.__m_activation_prime(self.__m_most_recent_output)
+        self.__m_most_recent_error = error
+
+        w = self.__m_weights
+        inputs = self.__m_most_recent_inputs
+        assert len(w) == len(inputs)
+
+        for i in range(len(w)):
+            w[i] -= self.__m_learning_rate * error * inputs[i]
+
+    def back_propogate(self, actual_value):
+        """
+        output layer back_prop
+        """
+        ## Possibly actual_value - self.__m_most_recent_output ??
+        error = self.__m_most_recent_output - actual_value
+        return self.__apply_error(error)
+
+    def most_recent_error(self):
+        return self.__m_most_recent_error
+
+    def back_propograte_hidden(self, next_layer, my_position):
+        error = 0.0
+        for neuron in next_layer:
+            error += neuron.weight(my_position) * \
+                neuron.most_recent_error()
+        return self.__apply_error(error)
+
